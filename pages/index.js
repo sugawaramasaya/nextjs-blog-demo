b626b7a802ca95c0a6020d48fc24da5480c5ea19
sugawara <sugawara@non-brokers.com>
2020-12-18 00:10:39 +0900

    Next.jsのImageコンポーネントはlayout="fill"を指定しない限り、widthとheightをpx指定しないとダメらしい。Theme UIのGridコンポーネントを追加

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import { jsx, Container } from 'theme-ui';
+import { jsx, Container, Grid, Box } from 'theme-ui';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Home = () => {
   return (
@@ -11,6 +12,71 @@ const Home = () => {
         <title>{siteTitle}</title>
       </Head>
       <Container>
+        <Grid gap={[0, 7]} columns={[1, 2, 4]}>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            ああああああああ
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            いいいい
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            ううううう
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            えええええええええ
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            おおおおおお
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            かかかかかかかかかかか
+          </Box>
+          <Box sx={{ bg: 'background' }}>
+            <Image
+              src="/images/dummy.jpg"
+              alt="Picture of the author"
+              width={768}
+              height={768}
+            />
+            きききき
+          </Box>
+        </Grid>
         <Link href="/works/css_pattern">
           <a>3種のCSS</a>
         </Link>
